bb99bdee71f47f53093e54d7480f41b8c0469b08
Jared <winkenj@sunypoly.edu>
2024-09-09 09:59:27 -0400

    Added Example_v2.py with it's assests. It is A combination of Example_v1 and Pygametest.

--- ScrollingBackground_Jared/Example_v2.py
+++ ScrollingBackground_Jared/Example_v2.py
@@ -22,14 +22,17 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SPEED = 5
 SCORE = 0
- 
+ACC = 0.5
+FRIC = -0.12
+vec = pygame.math.Vector2
+
 #Setting up Fonts
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
  
 #Create a white screen 
-DISPLAYSURF = pygame.display.set_mode((1200,800))
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
  
@@ -37,10 +40,10 @@ pygame.display.set_caption("Game")
 class Enemy(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
-        temp_image = pygame.image.load('wheel.png')
+        temp_image = pygame.image.load('enemy1.png')
         self.image = pygame.transform.scale(temp_image,(100,100))
-        self.surf = pygame.Surface((42, 70))
-        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH,random.randint(40,SCREEN_HEIGHT-40)))
+        self.surf = pygame.Surface((30, 30))
+        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH,700))
  
       def move(self):
         global SCORE
@@ -48,57 +51,62 @@ class Enemy(pygame.sprite.Sprite):
         if (self.rect.left < 0):
             SCORE += 1
             self.rect.left = 0
-            self.rect.center = (SCREEN_WIDTH,random.randint(40, SCREEN_HEIGHT - 40))
+            self.rect.center = (SCREEN_WIDTH,700)
  
  
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        temp_image = pygame.image.load('wheel.png')
+        temp_image = pygame.image.load('tire.gif')
         self.image = pygame.transform.scale(temp_image,(100,100))
-        self.surf = pygame.Surface((42, 70))
+        self.surf = pygame.Surface((30, 30))
         self.rect = self.surf.get_rect(center = (160, 520))
         
+        self.pos = vec((100, 360))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        
     def move(self):
+        self.acc = vec(0,0.5)
         pressed_keys = pygame.key.get_pressed()
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0,5)
-         
-        #if self.rect.left > 0:
-              #if pressed_keys[K_LEFT]:
-                  #self.rect.move_ip(-5, 0)
-        #if self.rect.right < SCREEN_WIDTH:        
-              #if pressed_keys[K_RIGHT]:
-                  #self.rect.move_ip(5, 0)
-'''
-# Vertical Background        
-class Background():
-      def __init__(self):
-            self.bgimage = pygame.image.load('image1.jpg')
-            self.rectBGimg = self.bgimage.get_rect()
- 
-            self.bgY1 = 0
-            self.bgX1 = 0
- 
-            self.bgY2 = self.rectBGimg.height
-            self.bgX2 = 0
- 
-            self.movingUpSpeed = 5
-         
-      def update(self):
-        self.bgY1 -= self.movingUpSpeed
-        self.bgY2 -= self.movingUpSpeed
-        if self.bgY1 <= -self.rectBGimg.height:
-            self.bgY1 = self.rectBGimg.height
-        if self.bgY2 <= -self.rectBGimg.height:
-            self.bgY2 = self.rectBGimg.height
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+        
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        
+        if self.pos.x > SCREEN_WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = 0
              
-      def render(self):
-         DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
-         DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
-'''
+        self.rect.midbottom = self.pos
+        
+    def jump(self):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if hits:
+           self.vel.y = -15
+    
+    def update(self):
+        hits = pygame.sprite.spritecollide(P1 ,platforms, False)
+        if P1.vel.y > 0:        
+            if hits:
+                self.vel.y = 0
+                self.pos.y = hits[0].rect.top + 1
+                
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        temp_image = pygame.image.load('road.png')
+        self.image = pygame.transform.scale(temp_image,(SCREEN_WIDTH,100))
+        self.surf = pygame.Surface((SCREEN_WIDTH,40))
+        self.rect = self.surf.get_rect(center = (SCREEN_WIDTH/2, 720))
+ 
+    def move(self):
+        pass
 
 # Horizontal Background
 class Background():
@@ -125,8 +133,9 @@ class Background():
       def render(self):
          DISPLAYSURF.blit(self.bgimage, (self.bgX1, self.bgY1))
          DISPLAYSURF.blit(self.bgimage, (self.bgX2, self.bgY2))
-         
-#Setting up Sprites        
+
+#Setting up Sprites
+PT1 = platform()      
 P1 = Player()
 E1 = Enemy()
  
@@ -136,8 +145,12 @@ back_ground = Background()
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
 all_sprites.add(P1)
 all_sprites.add(E1)
+
+platforms = pygame.sprite.Group()
+platforms.add(PT1)
  
 #Adding a new User event 
 INC_SPEED = pygame.USEREVENT + 1
@@ -149,7 +162,10 @@ while True:
     #Cycles through all occurring events   
     for event in pygame.event.get():
         if event.type == INC_SPEED:
-              SPEED += 0.5     
+              SPEED += 0.5
+        if event.type == pygame.KEYDOWN:    
+            if event.key == pygame.K_SPACE:
+                P1.jump()     
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -159,8 +175,10 @@ while True:
     back_ground.render()
  
     #DISPLAYSURF.blit(background, (0,0))
-    scores = font_small.render(str(SCORE), True, BLACK)
+    scores = font.render(str(SCORE), True, WHITE)
     DISPLAYSURF.blit(scores, (10,10))
+    
+    P1.update()
  
     #Moves and Re-draws all Sprites
     for entity in all_sprites:
