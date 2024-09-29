d4ca34cce7e2a75c0b5303aa6522013f5712ba89
Chakriya Sou <souc@sunypoly.edu>
2024-09-29 14:47:51 -0400

    Test - compute for players postion and platforms y coordinate

--- test/test_player.py
+++ test/test_player.py
@@ -8,6 +8,7 @@ import pytest
 from winkenj_files.player import Player
 from winkenj_files.platform import Platform
 import pygame
+import math
 
 pygame.init()
 
@@ -31,23 +32,22 @@ def screen_height():
 
 # Written by: Chakriya Sou
 # Created: 09/27/2024
-# Last update: 09/28/2024 2p (hard code position of player)
+# Last update: 09/29/2024 2p (Compute x,y coordinates)
 # Test: Player position (bottom left corner of screen) will 
 # adjust according to users display resolution
-# Will update in the future where x and y are not hard coded
 def test_Player(screen_width, screen_height):
     player = Player(screen_width, screen_height)
-    assert player.rect.center == (160, screen_height - 100)
+    assert player.rect.center == (math.ceil(screen_width * 0.1), screen_height * 0.75)
     #assert player.rect.center == (screen_width // 15, screen_height - 
     #                               (screen_height // 15))
 
 # Written by: Chakriya Sou
 # Created: 09/28/2024
+# Last update: 09/29/2024 2p (Compute y coordinates)
 # Test: Platform position (bottom of screen) will adjust according 
 # to users display resolution
-# Will update in the future where y is not hard coded
 def test_Platform(screen_width, screen_height):
     platform = Platform(screen_width, screen_height)
-    assert platform.rect.center == (screen_width // 2, screen_height - 50)
+    assert platform.rect.center == (screen_width // 2, math.ceil(screen_height * 0.95))
     #assert platform.rect.center == (screen_width // 2, screen_height - 
     #                               (screen_height * .075))
