cf43e632b658d7154a521a83ec9746d5cc41cd5b
Chakriya Sou <souc@sunypoly.edu>
2024-09-30 15:40:02 -0400

    Move test_platform to its own file and added pytest.ini (configuration files format)

--- test/test_platform.py
+++ test/test_platform.py
@@ -1,11 +1,10 @@
 # Written by: Chakriya Sou
-# Created: 09/27/2024 at 1:30p
+# Created: 09/30/2024 at 2:30p
 # Language: Python
 # Description: (pytest) These test functions will be implemented in
-# testing features in file player.py
+# testing features in file platform.py
 
 import pytest
-from winkenj_files.player import Player
 from winkenj_files.platform import Platform
 import pygame
 import math
@@ -31,10 +30,10 @@ def screen_height():
     return screen_height
 
 # Written by: Chakriya Sou
-# Created: 09/27/2024
-# Last update: 09/30/2024 2p (Players initial position was not on platform. Fixed problem)
-# Test: Player position (bottom left corner of screen) will 
-# adjust according to users display resolution
-def test_Player(screen_width, screen_height):
-    player = Player(screen_width, screen_height)
-    assert player.rect.center == (math.ceil(screen_width * 0.1), math.ceil(screen_height * 0.95))
+# Created: 09/28/2024
+# Last update: 09/29/2024 2p (Compute y coordinates)
+# Test: Platform position (bottom of screen) will adjust according 
+# to users display resolution
+def test_Platform(screen_width, screen_height):
+    platform = Platform(screen_width, screen_height)
+    assert platform.rect.center == (screen_width // 2, math.ceil(screen_height * 0.95))
